fe67b53dcbe276f592c12751ecd17b5d25321b21
therevolvingkitchen <imgamehmet@gmail.com>
2017-11-25 16:57:19 -0500

    contact us added

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,19 +1,15 @@
 class MessageMailer < ActionMailer::Base
-    
+
       default from: "TRK contact us <therevolvingkitchen@gmail.com>"
       default to: "therevolvingkitchen@gmail.com"
-      
+
       layout 'new_message'
-      
+
       def new_message(message)
         @message = message
-        
+
         mail subject: "Message from #{message.name}"
 
-        #mail(to: "mimga74@yahoo.com",
-        #body: message.name,
-        #content_type: "text/html",
-        #subject: "Already rendered!")
       end
-    
+
     end
