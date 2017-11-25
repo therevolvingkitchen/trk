41b415fd1bdd8552341af7adb9bd62305f398fb7
therevolvingkitchen <imgamehmet@gmail.com>
2017-11-25 16:49:28 -0500

    contact us added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,30 @@
 class MessagesController < ApplicationController
-    
+
       def new
         @message = Message.new
       end
-    
+
       def create
         @message = Message.new(message_params)
-        
+
         if @message.valid?
           MessageMailer.new_message(@message).deliver
-          redirect_to contact_path, notice: "Your messages has been sent."
+          render :json => {
+            :type => "success",
+            :message => "Your messages has been sent."
+          }.to_json
         else
-          flash[:alert] = "An error occurred while delivering this message."
-          render :new
+          render :json => {
+            :type => "danger",
+            :message => "An error occurred while delivering this message."
+          }.to_json
         end
       end
-    
+
     private
-    
+
       def message_params
         params.require(:message).permit(:name, :email, :surname, :phone, :message)
       end
-    
+
 end
